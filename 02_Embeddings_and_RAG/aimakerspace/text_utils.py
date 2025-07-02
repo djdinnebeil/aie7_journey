2737e9d203c4a7dd192a51a9fd93eed1a2b5fe9c
DJ <djdinnebeil@gmail.com>
2025-07-02 16:20:15 -0400

    Checkpoint for session 02

--- 02_Embeddings_and_RAG/aimakerspace/text_utils.py
+++ 02_Embeddings_and_RAG/aimakerspace/text_utils.py
@@ -6,6 +6,30 @@ import pytesseract
 from docx import Document
 import textract
 
+class CharacterTextSplitter:
+    def __init__(
+        self,
+        chunk_size: int = 1000,
+        chunk_overlap: int = 200,
+    ):
+        assert (
+            chunk_size > chunk_overlap
+        ), "Chunk size must be greater than chunk overlap"
+
+        self.chunk_size = chunk_size
+        self.chunk_overlap = chunk_overlap
+
+    def split(self, text: str) -> List[str]:
+        chunks = []
+        for i in range(0, len(text), self.chunk_size - self.chunk_overlap):
+            chunks.append(text[i : i + self.chunk_size])
+        return chunks
+
+    def split_texts(self, texts: List[str]) -> List[str]:
+        chunks = []
+        for text in texts:
+            chunks.extend(self.split(text))
+        return chunks
 
 class BaseFileLoader:
     def __init__(self, path: str):
@@ -53,32 +77,6 @@ class TextFileLoader(BaseFileLoader):
     def load_file(self, file_path: str):
         with open(file_path, "r", encoding=self.encoding) as f:
             self.documents.append(f.read())
-
-
-class CharacterTextSplitter:
-    def __init__(
-        self,
-        chunk_size: int = 1000,
-        chunk_overlap: int = 200,
-    ):
-        assert (
-            chunk_size > chunk_overlap
-        ), "Chunk size must be greater than chunk overlap"
-
-        self.chunk_size = chunk_size
-        self.chunk_overlap = chunk_overlap
-
-    def split(self, text: str) -> List[str]:
-        chunks = []
-        for i in range(0, len(text), self.chunk_size - self.chunk_overlap):
-            chunks.append(text[i : i + self.chunk_size])
-        return chunks
-
-    def split_texts(self, texts: List[str]) -> List[str]:
-        chunks = []
-        for text in texts:
-            chunks.extend(self.split(text))
-        return chunks
 
 
 class PDFFileLoader(BaseFileLoader):
